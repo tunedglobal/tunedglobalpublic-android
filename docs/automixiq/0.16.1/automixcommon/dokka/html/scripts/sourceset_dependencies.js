54449f2d55034f81aa77636ae5d32ba49c152f52
Pacemaker <pacemaker@Vics-MacBook-Pro.local>
2025-04-04 16:47:18 +0200

    Added/Updated version 0.16.1

---
+++ docs/automixiq/0.16.1/automixcommon/dokka/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":AutomixCommon:dokkaHtml/debug":[],":AutomixCommon:dokkaHtml/distributable":[],":AutomixCommon:dokkaHtml/main":[],":AutomixCommon:dokkaHtml/release":[]}'
---
+++ docs/automixiq/0.16.1/automixiq/dokka/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":AutomixIQ:dokkaHtml/debug":[],":AutomixIQ:dokkaHtml/distributable":[],":AutomixIQ:dokkaHtml/main":[],":AutomixIQ:dokkaHtml/release":[]}'
